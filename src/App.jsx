1ab425bbf63c41ce1c8b51367e5f3f6046d300e8
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-02-05 21:52:40 +0530

    add utility functions and update project configuration
    - Introduced a new utility function `cn` in utils.js for class name management.
    - Updated App component to remove unnecessary class name.
    - Added new dependencies: clsx, tailwind-merge, and motion.
    - Modified Vite configuration to include autoprefixer and updated Tailwind CSS integration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1 className=''>hello</h1>
+      <h1>hello</h1>
     </>
   )
 }
