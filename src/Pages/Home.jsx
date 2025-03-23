a5f6059c654d6358f5d540e1bbc88eb20c3e565e
abhy <b23cs1085@iitj.ac.in>
2025-03-23 23:06:21 +0530

    refactor import path for Carousel component in Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Carousel from './Carousel'; 
+import Carousel from '../Components/Carousel'; 
 import AnimeCarousel from '../Components/TrendsCard';
 
 function Home() {
